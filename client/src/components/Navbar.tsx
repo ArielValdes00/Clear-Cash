d6f6b885245785d108bbdc2e61afba7f7eb1800a
ArielValdes00 <124757872+ArielValdes00@users.noreply.github.com>
2023-10-26 22:08:38 -0300

    actualizacion carpeta client

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BsFillMoonFill, BsFillSunFill, BsCoin } from 'react-icons/bs';
+
+interface ThemeProps {
+    handleChangeTheme: () => void
+    toggleButton: boolean
+}
+
+const Navbar: React.FC<ThemeProps> = ({ handleChangeTheme, toggleButton }) => {
+    return (
+        <div className='grid grid-cols-3 md:grid-cols-9 items-center px-4 lg:px-5 py-1'>
+            <div className='flex gap-1 text-sm text-color-green font-bold uppercase'>
+                <BsCoin size={24} />
+                <div className='flex flex-col leading-3'>
+                    <p>Clear</p>
+                    <p>Cash</p>
+                </div>
+            </div>
+            <div className='mt-[15px] mx-auto md:ml-auto lg:ml-12 xl:ml-24 md:col-start-8'>
+                <label
+                    htmlFor={'check'}
+                    className={'py-2 cursor-pointer relative mb-3 w-[75px] lg:w-20 h-[33px] shadow lg:h-9 rounded-full flex items-center border-gray-300 dark:border-color-green border gap-2'}
+                >
+                    <input
+                        type='checkbox'
+                        id={'check'}
+                        className='sr-only peer'
+                        onClick={handleChangeTheme}
+                    />
+                    <span className='w-[27px] lg:w-[28px] shadow h-[27px] lg:h-[28px] bg-color-green absolute rounded-full left-[3px] lg:top-[3px] top-[2px] peer-checked:left-[44px] lg:peer-checked:left-[47px] transition-all duration-500'>
+                        <span className='absolute text-gray-100 dark:text-black top-[4px] lg:top-1 left-[4px]'>
+                            {toggleButton ? <BsFillMoonFill size={19} /> : <BsFillSunFill size={19} />}
+                        </span>
+                    </span>
+                </label>
+            </div>
+            <button className='shadow ml-auto md:col-start-9 px-4 lg:py-2 py-[6px] font-semibold rounded-md bg-color-green text-gray-100 dark:text-black text-sm hover:bg-opacity-80'>Log In</button>
+        </div>
+
+    );
+};
+
+export default Navbar;
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,36 @@
+import Navbar from '@/components/Navbar';
+import { useEffect, useState } from 'react';
+import { FaArrowRight } from 'react-icons/fa';
+
+export default function Home() {
+    const [theme, setTheme] = useState<string>('');
+    const [toggleButton, setToggleButton] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (theme === 'dark') {
+            document.querySelector('html')?.classList.add('dark');
+        } else {
+            document.querySelector('html')?.classList.remove('dark');
+        }
+    }, [theme]);
+
+    const handleChangeTheme = (): void => {
+        setTheme((prevTheme: string) => prevTheme === 'dark' ? 'light' : 'dark');
+        setToggleButton(!toggleButton);
+    };
+    return (
+        <div className='bg-gray-200 dark:bg-black flex flex-col h-screen'>
+            <Navbar handleChangeTheme={handleChangeTheme} toggleButton={toggleButton}/>
+            <div className='mb-5 flex flex-col max-w-[350px] sm:max-w-sm lg:max-w-md text-center mx-auto flex-grow justify-center gap-6 items-center'>
+                <h1 className='text-6xl lg:text-7xl font-extrabold text-color-green shadow-text'>Take control of your finances</h1>
+                <p className='max-w-xs lg:max-w-md dark:text-gray-400 text-[15px] text-gray-500'>
+                    With ClearCash, managing your expenses has never been easier. Track your spending, set budgets, and achieve your financial goals all in one place.
+                </p>
+                <div className='cursor-pointer text-gray-100 dark:text-black shadow flex items-center gap-3 px-7 py-4 rounded-full font-extrabold lg:font-bold bg-color-green uppercase hover:bg-opacity-80'>
+                    <p>Get Started</p>
+                    <FaArrowRight size={17} />
+                </div>
+            </div>
+        </div>
+    );
+}
